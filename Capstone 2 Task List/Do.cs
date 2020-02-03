8c73b09141c7db58433770c621cc185da807f39a
Kyle Vaughn <kcvaughn75@gmail.com>
2020-02-03 08:47:33 -0500

    First commit for Capstone2

---
+++ Capstone 2 Task List/Do.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone_2_Task_List
+{
+    class Do
+    {
+        private string name;
+        private string task;
+        private string due;
+        private string description;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Task
+        {
+            get { return task; }
+            set { task = value; }
+        }
+
+        public string Due
+        {
+            get { return due; }
+            set { due = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public Do()
+        {
+
+        }
+
+        public Do(string _name, string _task, string _due, string _description)
+        {
+            name = _name;
+            task = _task;
+            due = _due;
+            description = _description;
+        }
+
+        public static void PrintTaskList(List<string> list1, List<string> list2)
+        {
+            for (int i = 0; i < list1.Count; i++)
+            {
+                Console.WriteLine($"{");
+            }
+        }
+        
+
+    }
+}
---
+++ Capstone 2 Task List/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone_2_Task_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Do> grandCircusTasks = new List<Do>
+            {
+                new Do("aaron", "alpha", "03/01/2020", "update alpha program" ),
+                new Do("brian", "bravo", "03/02/2020", "update beta program"),
+                new Do("Cherie", "charlie", "03/03/2020", "update charlie program"),
+            };
+
+            Console.WriteLine("Welcome to the Task Manager!");
+            Console.WriteLine(" 1. List tasks \n 2. Add task \n 3. Delete Task \n 4. Mark Task Complete \n 5. Quit");
+            
+            string menuSelection = "";
+            bool tryAgain = true;
+
+            while (tryAgain)
+            {
+                string userInput = Console.ReadLine().ToLower();
+                tryAgain = false;
+                switch (userInput)
+                {
+                    case "1":
+                    case "list task":
+                        menuSelection = "list task";
+                        break;
+
+                    case "2":
+                    case "add task":
+                        menuSelection = "add task";
+                        break;
+
+                    case "3":
+                    case "delete task":
+                        menuSelection = "delete task";
+                        break;
+
+                    case "4":
+                    case "mark task complete":
+                        menuSelection = "mark task complete";
+                        break;
+                    case "5":
+                    case "quit":
+                        break;
+                    default:
+                        tryAgain = true;
+                        Console.WriteLine("Invalid. Please enter a valid entry number from the menu.");
+                        break;
+
+                }
+            }
+            if (menuSelection == "1" || menuSelection == "list task")
+            {
+                Do.PrintTaskList(name);
+            }
+            
+        }
+        
+    }
+}
